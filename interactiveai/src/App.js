3269aede752460917fb63b00f3916609f786e056
Morad <mabo0003@student.monash.edu>
2022-08-17 17:21:15 +0300

    further landing page implementation similar to designed figma

--- interactiveai/src/App.js
+++ interactiveai/src/App.js
@@ -1,9 +1,11 @@
 function App() {
   return (
-<div className="min-h-screen flex flex-col text-black">
+<div className="min-h-screen text-black" style={{
+        display: 'flex',
+        alignItems: 'center'}}>
 			<main className="container mx-auto px-6 pt-16 flex-1 text-center">
-
-				<h2 className="text-2xl md:text-4xl lg:text-4xl uppercase">Welcome to our</h2>
+        <center><img src={require('./scale.png')} alt = "" /></center>
+				<h2 className="text-2xl md:text-4xl lg:text-3xl uppercase">Welcome to our</h2>
 				<h1 className="text-3xl md:text-6xl lg:text-6xl uppercase font-black mb-8">AI Ethics Quiz</h1>
 
 				<form 
@@ -11,22 +13,24 @@ function App() {
 					method="post" 
 					id="revue-form"
 					name="revue-form"
-					target="_blank">
+          style={{display: 'flex',alignItems: 'center',
+          justifyContent: 'center'}} size='' 
+          >
 
-					<div className="flex flex-col md:flex-row justify-center mb-4">
+					<div className="flex flex-col">
 						<input
 							placeholder="Your email address..."
 							type="email"
 							name="member[email]"
 							id="member_email"
-							className="text-2xl placeholder:text-gray-400 placeholder:italic py-4 px-6 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 focus:bg-opacity-20 duration-150 md:rounded-tr-none md:rounded-br-none rounded-full outline-none mb-4 md:mb-0"
+							className="text-2xl placeholder:text-gray-400 placeholder:italic py-4 px-6 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-50 focus:bg-opacity-75 hover:bg-opacity-75 duration-150 md:rounded-full outline-none mb-4"
 						/>
 						<input
 							type="submit"
 							value="Let's start!"
 							name="member[submit]"
 							id="member_submit"
-							className="bg-primary md:rounded-tl-none md:rounded-bl-none rounded-full text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold uppercase cursor-pointer hover:opacity-75 duration-150"
+							className="bg-primary bg-black bg-opacity-30 md:rounded outline-none mb-4 text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold uppercase"
 						/>
 					</div>
 				</form>
