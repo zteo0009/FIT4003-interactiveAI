477c53d84cb87d1f13bd5661a6d16b5bddd1caa3
Morad <mabo0003@student.monash.edu>
2022-08-17 14:35:21 +0300

    Basic landing page

--- interactiveai/src/App.js
+++ interactiveai/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="min-h-screen flex flex-col text-black">
+			<main className="container mx-auto px-6 pt-16 flex-1 text-center">
+
+				<h2 className="text-2xl md:text-4xl lg:text-4xl uppercase">Welcome to our</h2>
+				<h1 className="text-3xl md:text-6xl lg:text-6xl uppercase font-black mb-8">AI Ethics Quiz</h1>
+
+				<form 
+					action= "" //Need to put action here
+					method="post" 
+					id="revue-form"
+					name="revue-form"
+					target="_blank">
+
+					<div className="flex flex-col md:flex-row justify-center mb-4">
+						<input
+							placeholder="Your email address..."
+							type="email"
+							name="member[email]"
+							id="member_email"
+							className="text-2xl placeholder:text-gray-400 placeholder:italic py-4 px-6 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 focus:bg-opacity-20 duration-150 md:rounded-tr-none md:rounded-br-none rounded-full outline-none mb-4 md:mb-0"
+						/>
+						<input
+							type="submit"
+							value="Let's start!"
+							name="member[submit]"
+							id="member_submit"
+							className="bg-primary md:rounded-tl-none md:rounded-bl-none rounded-full text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold uppercase cursor-pointer hover:opacity-75 duration-150"
+						/>
+					</div>
+				</form>
+			</main>
+		</div>
   );
 }
 
