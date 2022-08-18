ffda39fecbbbfe4c25280d0cc36be5fdf3b00367
Teo Wei Sheng <wteo0011@student.monash.edu>
2022-08-18 21:18:11 +0800

    Added routing between the different pages

--- interactiveai/src/App.js
+++ interactiveai/src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from './views/Homepage'
+import Questions from './views/Questions'
+import Result from './views/Result'
 
 function App() {
   return (
-	<Homepage />
+	<Router>
+		<Switch>
+			<Route path="/" exact>
+				<Homepage />
+			</Route>
+			<Route path="/questions">
+				<Questions />
+			</Route>
+			<Route path="/result">
+				<Result />
+			</Route>
+		</Switch>
+	</Router>
+	
   );
 }
 
