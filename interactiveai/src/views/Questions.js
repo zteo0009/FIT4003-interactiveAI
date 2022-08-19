fe9925c9a3c733dde938b0afbe9e6da0de18539d
Teo Wei Sheng <wteo0011@student.monash.edu>
2022-08-19 15:39:30 +0800

    Added code for splitting screen

--- interactiveai/src/views/Questions.js
+++ interactiveai/src/views/Questions.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Questions = () => {
   return (
-    <div>Questions</div>
+    <React.Fragment>
+      <section>
+        <div className='w-screen h-screen grid grid-rows-2 text-black text-4xl md:grid-cols-2'>
+          <div className=' w-full h-full centered md:h-screen'>
+            <p>Question</p>
+          </div>
+
+          {/* page 2 */}
+          <div className='w-full h-full bg-white centered md:h-screen'>
+            <p>Options</p>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
   )
 }
 
