90c2196f910151f414ec2c8f4f01c9542c4772bb
Teo Wei Sheng <wteo0011@student.monash.edu>
2022-08-20 17:13:12 +0800

    Switching questions when answer is selected

--- interactiveai/src/components/QuizOptions.js
+++ interactiveai/src/components/QuizOptions.js
@@ -3,17 +3,21 @@ function QuizOptions(props) {
         <div className="min-h-screen text-black" style={{ display: 'flex', alignItems: 'center' }}>
             <main className="container mx-auto px-6 pt-16 flex-1 text-center">
                 <button
+                    onClick={() => props.onSelectOption()}
                     className="w-5/6 bg-primary bg-black hover:bg-opacity-50 bg-opacity-30 md:rounded outline-none mb-8 text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold"
-                >Option A</button>
+                >{props.options.answerOptions[0].answerText}</button>
                 <button
+                    onClick={() => props.onSelectOption()}
                     className="w-5/6 bg-primary bg-black hover:bg-opacity-50 bg-opacity-30 md:rounded outline-none mb-8 text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold"
-                >Option B</button>
+                >{props.options.answerOptions[1].answerText}</button>
                 <button
+                    onClick={() => props.onSelectOption()}
                     className="w-5/6 bg-primary bg-black hover:bg-opacity-50 bg-opacity-30 md:rounded outline-none mb-8 text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold"
-                >Option C</button>
+                >{props.options.answerOptions[2].answerText}</button>
                 <button
+                    onClick={() => props.onSelectOption()}
                     className="w-5/6 bg-primary bg-black hover:bg-opacity-50 bg-opacity-30 md:rounded outline-none mb-8 text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold"
-                >Option D</button>
+                >{props.options.answerOptions[3].answerText}</button>
             </main>
         </div>
     )
--- interactiveai/src/views/Questions.js
+++ interactiveai/src/views/Questions.js
@@ -9,20 +9,60 @@ const Questions = () => {
       questionno: 1,
 			scenario: 'You ate a piece of bread.',
 			answerOptions: [
-				{ answerText: 'Be full', isCorrect: false },
-				{ answerText: 'Be happy', isCorrect: false },
-				{ answerText: 'Be sad', isCorrect: false },
-				{ answerText: 'Die', isCorrect: true },
+				{ answerText: 'Be full', score: 0 },
+				{ answerText: 'Be happy', score: 1 },
+				{ answerText: 'Be sad', score: 2 },
+				{ answerText: 'Die', score: 3 },
 			],
 		},
 		{
       questionno: 2,
 			scenario: 'Who is CEO of Tesla?',
 			answerOptions: [
+				{ answerText: 'Jeff Bezos', score: 0 },
+				{ answerText: 'Elon Musk', score: 1 },
+				{ answerText: 'Bill Gates', score: 2 },
+				{ answerText: 'Tony Stark', score: 3 },
+			],
+		},
+    {
+      questionno: 3,
+			scenario: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionno: 4,
+			scenario: 'Who is CEO of Tesla?',
+			answerOptions: [
 				{ answerText: 'Jeff Bezos', isCorrect: false },
 				{ answerText: 'Elon Musk', isCorrect: true },
 				{ answerText: 'Bill Gates', isCorrect: false },
 				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+      questionno: 5,
+			scenario: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionno: 6,
+			scenario: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
 			],
 		},
 	];
@@ -39,7 +79,7 @@ const Questions = () => {
 
           {/* page 2 */}
           <div className='w-full h-full centered md:h-screen'>
-            <QuizOptions />
+            <QuizOptions options={questions[currentQuestion]} onSelectOption={()=>{setCurrentQuestion(currentQuestion + 1)}}/>
           </div>
         </div>
       </section>
