2352dbe2b1d5f3bc70a874e47976226cb4b10c16
Morad <mabo0003@student.monash.edu>
2022-08-24 15:39:27 +0300

    Initialized firebase in the app

--- interactiveai/src/index.js
+++ interactiveai/src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {initializeApp} from 'firebase/app';
+import { getFirestore, getDoc, doc} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNiSXkYBtG_SUwu7RV3Qxpiur3WiIJQpI",
+  authDomain: "fit4003-interactiveai.firebaseapp.com",
+  projectId: "fit4003-interactiveai",
+  storageBucket: "fit4003-interactiveai.appspot.com",
+  messagingSenderId: "526897660959",
+  appId: "1:526897660959:web:047283572ee7dbf182c12a",
+  measurementId: "G-M236MH6LSV"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
