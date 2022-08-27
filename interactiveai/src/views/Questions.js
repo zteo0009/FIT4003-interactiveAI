c30a9705bf3ab60ab1a345944fda2cc7a01c9e21
Teo Wei Sheng <wteo0011@student.monash.edu>
2022-08-27 20:08:08 +0800

    Changed options to use options from firestore

--- interactiveai/src/views/Questions.js
+++ interactiveai/src/views/Questions.js
@@ -8,70 +8,7 @@ import { collection, getDocs, orderBy, query } from "firebase/firestore";
 
 const Questions = () => {
 
-	const questions = [
-		{
-			questionno: 1,
-			scenario: 'You ate a piece of bread.',
-			answerOptions: [
-				{ answerText: 'Be full', score: 0 },
-				{ answerText: 'Be happy', score: 1 },
-				{ answerText: 'Be sad', score: 2 },
-				{ answerText: 'Eat more and become fat', score: 3 },
-			],
-		},
-		{
-			questionno: 2,
-			scenario: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', score: 0 },
-				{ answerText: 'Elon Musk', score: 1 },
-				{ answerText: 'Bill Gates', score: 2 },
-				{ answerText: 'Tony Stark', score: 3 },
-			],
-		},
-		{
-			questionno: 3,
-			scenario: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', score: 0 },
-				{ answerText: 'London', score: 1 },
-				{ answerText: 'Paris', score: 2 },
-				{ answerText: 'Dublin', score: 3 },
-			],
-		},
-		{
-			questionno: 4,
-			scenario: 'Who is the most handsome man alive?',
-			answerOptions: [
-				{ answerText: 'daro', score: 0 },
-				{ answerText: 'daro', score: 1 },
-				{ answerText: 'daro', score: 2 },
-				{ answerText: 'daro', score: 3 },
-			],
-		},
-		{
-			questionno: 5,
-			scenario: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', score: 0 },
-				{ answerText: 'Intel', score: 1 },
-				{ answerText: 'Amazon', score: 2 },
-				{ answerText: 'Microsoft', score: 3 },
-			],
-		},
-		{
-			questionno: 6,
-			scenario: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', score: 0 },
-				{ answerText: '4', score: 1 },
-				{ answerText: '6', score: 2 },
-				{ answerText: '7', score: 3 },
-			],
-		},
-	];
-
-	const [questionss, setQuestions] = useState([]);
+	const [questions, setQuestions] = useState([]);
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const navigate = useNavigate();
 
@@ -104,7 +41,7 @@ const Questions = () => {
 				});
 				setQuestions(temp);
 			});
-			
+
 		}
 
 		getQuestions();
@@ -117,16 +54,18 @@ const Questions = () => {
 					<img className="fixed mx-auto bottom-0 object-center" src={require('../images/ellipse-2.png')} alt="" />
 					<img className="fixed top-30 left-0" src={require('../images/ellipse-1.png')} alt="" />
 					{/* Left part */}
-					{questionss && questionss.length > 0 &&
+					{questions && questions.length > 0 &&
 						<div className='questions-bg w-full h-full centered md:h-screen'>
-							<Question question={questionss[currentQuestion]} />		
+							<Question question={questions[currentQuestion]} />
 						</div>
 					}
 
 					{/* Right part */}
-					<div className='w-full h-full centered md:h-screen'>
-						{/* <QuizOptions options={questions[currentQuestion]} onSelectOption={() => { handleAnswerSelect() }} /> */}
-					</div>
+					{questions && questions.length > 0 &&
+						<div className='w-full h-full centered md:h-screen'>
+							<QuizOptions options={questions[currentQuestion]} onSelectOption={() => { handleAnswerSelect() }} />
+						</div>
+					}
 				</motion.div>
 			</section>
 		</React.Fragment >
