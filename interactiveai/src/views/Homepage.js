c4e1496354d02a06103319102032a6e409509b77
Teo Wei Sheng <wteo0011@student.monash.edu>
2022-08-27 20:36:12 +0800

    Changed width of robot hand

--- interactiveai/src/views/Homepage.js
+++ interactiveai/src/views/Homepage.js
@@ -7,21 +7,21 @@ function Homepage() {
 
     const navigate = useNavigate();
 
-    const routeChange = () =>{ 
-        let path = '/questions'; 
+    const routeChange = () => {
+        let path = '/questions';
         navigate(path);
     }
 
     return (
-    <>
-     <img className="fixed right-0" src={require('../images/top-right-sphere.png')} alt="" />
-     <img className="fixed bottom-0 right-0 w-500" src={require('../images/bottom-sphere.png')} alt="" />
-     <img className="fixed left-0" src={require('../images/top-blob.png')} alt="" />
-     <img className="fixed bottom-0 right-50" src={require('../images/bottom-blob.png')} alt="" />
-  
-       <motion.div className="min-h-screen text-black" style={{alignItems: 'center' }} initial={{width : 0}} animate={{width : "100%"}} exit={{x : window.innerWidth, transition: {duration: 0.2}}}>
-      
-            <div className="flex h-screen justify-center items-center space-x-10 ">
+        <>
+            <img className="fixed right-0" src={require('../images/top-right-sphere.png')} alt="" />
+            <img className="fixed bottom-0 right-0 w-500" src={require('../images/bottom-sphere.png')} alt="" />
+            <img className="fixed left-0" src={require('../images/top-blob.png')} alt="" />
+            <img className="fixed bottom-0 right-50" src={require('../images/bottom-blob.png')} alt="" />
+
+            <motion.div className="min-h-screen text-black" style={{ alignItems: 'center' }} initial={{ width: 0 }} animate={{ width: "100%" }} exit={{ x: window.innerWidth, transition: { duration: 0.2 } }}>
+
+                <div className="flex h-screen justify-center items-center space-x-10 ">
                     <div className="max-w-xl">
                         <h3>Welcome to our</h3>
                         <h1>AI ETHICS QUIZ</h1>
@@ -35,16 +35,15 @@ function Homepage() {
                             onClick={routeChange}>Let's start!</button>
                     </div>
                     <div className="empty"></div>
-             <div className="robot-hand">
-                <img src={require('../images/robot-arm.png')} alt="" />
-             </div>
-            </div>      
+                    <div className="robot-hand">
+                        <img src={require('../images/robot-arm.png')} alt="" />
+                    </div>
+                </div>
 
 
-        </motion.div>
+            </motion.div>
         </>
 
-       
     );
 }
 
