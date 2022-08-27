51fe310cc25464cb2c5ec20e6745a2f24f8fde2f
Teo Wei Sheng <wteo0011@student.monash.edu>
2022-08-27 18:51:57 +0800

    Changed question page to use questions from firestore

--- interactiveai/src/components/Question.js
+++ interactiveai/src/components/Question.js
@@ -2,10 +2,10 @@ function Question(props) {
     return (
         <div className="min-h-screen text-black" style={{ display: 'flex', alignItems: 'center' }}>
             <main className="container mx-auto px-6 pt-16 flex-1 text-center">
-                <h3 className="text-lg text-black md:text-lg lg:text-lg uppercase mb-16">Question {props.question.questionno}/6</h3>
+                {/* <h3 className="text-lg text-black md:text-lg lg:text-lg uppercase mb-16">Question {props.question.questionno}/6</h3> */}
                 <h2 className="text-3xl md:text-6xl lg:text-4xl uppercase font-bold mb-16">Given the scenario...</h2>
                 <p className="text-lg text-black md:text-lg lg:text-lg mb-16">{props.question.scenario}</p>
-                <h3 className="text-lg text-black md:text-lg lg:text-lg">What would you do?</h3>
+                <h3 className="text-lg text-black md:text-lg lg:text-lg">{props.question.question}</h3>
             </main>
         </div>
     )
--- interactiveai/src/views/QuestionsOld.js
+++ interactiveai/src/views/QuestionsOld.js
@@ -1,10 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Question from '../components/Question'
 import QuizOptions from '../components/QuizOptions'
 import { useNavigate } from "react-router-dom";
 import { motion } from 'framer-motion';
-import { db } from '../index'
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
 
 const Questions = () => {
 
@@ -71,7 +69,6 @@ const Questions = () => {
 		},
 	];
 
-	const [questionss, setQuestions] = useState([]);
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const navigate = useNavigate();
 
@@ -86,54 +83,27 @@ const Questions = () => {
 		}
 	}
 
-	useEffect(() => {
-		const getQuestions = async () => {
-			const temp = [];
-			const questionsCollectionRef = collection(db, "Scenario")
-			const documents = await getDocs(query(questionsCollectionRef, orderBy("Scenariono")));
-
-			documents.forEach(async collectionDoc => {
-				const scenario = collectionDoc.data().Background;
-				let subCollectionDocs = await getDocs(collection(db, "Scenario", collectionDoc.id, "Questions"));
-				subCollectionDocs.forEach(subCollectionDoc => {
-					temp.push({
-						scenario: scenario,
-						question: subCollectionDoc.data().Question,
-						answerOptions: subCollectionDoc.data().AnswerOptions
-					})
-				});
-				setQuestions(temp);
-			});
-			
-		}
-
-		getQuestions();
-	}, [])
-
 	return (
-		< React.Fragment >
+		<React.Fragment>
+
 			<section>
-				<motion.div className='w-screen h-screen grid grid-rows-2 text-black text-4xl md:grid-cols-2' initial={{ width: 0 }} animate={{ width: "100%" }} exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}>
+				
+				<motion.div className='w-screen h-screen grid grid-rows-2 text-black text-4xl md:grid-cols-2' initial={{width : 0}} animate={{width : "100%"}} exit={{x : window.innerWidth, transition: {duration: 0.1}}}>
 					<img className="fixed mx-auto bottom-0 object-center" src={require('../images/ellipse-2.png')} alt="" />
 					<img className="fixed top-30 left-0" src={require('../images/ellipse-1.png')} alt="" />
 					{/* Left part */}
-					{questionss && questionss.length > 0 &&
-						<div className='questions-bg w-full h-full centered md:h-screen'>
-							<Question question={questionss[currentQuestion]} />		
-						</div>
-					}
+					<div className='questions-bg w-full h-full centered md:h-screen'>
+						<Question question={questions[currentQuestion]} />
+					</div>
 
 					{/* Right part */}
 					<div className='w-full h-full centered md:h-screen'>
-						{/* <QuizOptions options={questions[currentQuestion]} onSelectOption={() => { handleAnswerSelect() }} /> */}
+						<QuizOptions options={questions[currentQuestion]} onSelectOption={() => { handleAnswerSelect() }} />
 					</div>
 				</motion.div>
 			</section>
-		</React.Fragment >
-
+		</React.Fragment>
 	)
-	// 	})
-	// )
 }
 
 export default Questions
