1c0907f9e71ed947677835ea77620241d9043ff6
Dayang Intan <daba0003@student.monash.edu>
2022-08-29 22:57:48 +0800

    Update questions page - modified css of left question panel

--- interactiveai/src/views/Questions.js
+++ interactiveai/src/views/Questions.js
@@ -60,14 +60,17 @@ const Questions = () => {
 	return (
 		< React.Fragment >
 			<section>
-				<motion.div className='w-screen h-screen grid grid-rows-2 text-black text-4xl md:grid-cols-2' initial={{ width: 0 }} animate={{ width: "100%" }} exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}>
+				<motion.div className='w-screen grid text-black text-4xl md:grid-cols-2' initial={{ width: 0 }} animate={{ width: "100%" }} exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}>
 					<img className="fixed mx-auto bottom-0 object-center" src={require('../images/ellipse-2.png')} alt="" />
 					<img className="fixed top-30 left-0" src={require('../images/ellipse-1.png')} alt="" />
 					{/* Left part */}
 					{questions && questions.length > 0 &&
-						<div className='questions-bg w-full h-full centered md:h-screen'>
+					<div className="questions-parent max-h-screen">
+						<div className='questions-bg'>
 							<Question question={questions[currentQuestion]} />
 						</div>
+					</div>
+						
 					}
 
 					{/* Right part */}
