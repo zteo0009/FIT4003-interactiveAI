c695d1222d1dd20e85aade3c840372d6e6aaedb4
Dayang <daba0003@student.monash.edu>
2022-08-31 11:21:39 +0800

    Added sample illustration on quiz pages

--- interactiveai/src/components/Question.js
+++ interactiveai/src/components/Question.js
@@ -2,9 +2,12 @@ function Question(props) {
     return (
         <div className="text-black" style={{ display: 'flex', alignItems: 'center' }}>
             <main className="container flex-1 text-center">
-                <h3 className="text-lg text-black md:text-lg lg:text-lg uppercase mb-16">Question {props.question.questionno}/x</h3>
-                <h2 className="text-3xl md:text-6xl lg:text-4xl uppercase font-bold mb-16">Given the scenario...</h2>
-                <p className="text-lg text-black md:text-lg lg:text-lg mb-16">{props.question.scenario}</p>
+                <h3 className="text-base text-black md:text-base lg:text-base uppercase mb-2">Question {props.question.questionno}/x</h3>
+                <h2 className="text-3xl md:text-6xl lg:text-4xl uppercase font-bold mb-6">Given the scenario...</h2>
+                <div className="illustration mb-6">
+                    <img src={require('../images/sample-illustration.png')} alt="" />
+                </div>
+                <p className="text-base text-black md:text-lg lg:text-base text-justify mb-6 ">{props.question.scenario}</p>
                 <h3 className="text-lg text-black md:text-lg lg:text-lg">{props.question.question}</h3>
             </main>
         </div>
