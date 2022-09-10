547458876f6ac846cd26b3ab4510fe39d8b7e6be
Teo Wei Sheng <wteo0011@student.monash.edu>
2022-09-10 14:37:08 +0800

    Pass data obtained from firestore from questions page to result page
    (instead of getting it again in result page)

--- interactiveai/src/views/Questions.js
+++ interactiveai/src/views/Questions.js
@@ -8,6 +8,24 @@ import { collection, getDocs, orderBy, query } from "firebase/firestore";
 
 let scoreList = [];
 
+function shuffle(array) {
+	let currentIndex = array.length, randomIndex;
+
+	// While there remain elements to shuffle.
+	while (currentIndex != 0) {
+
+		// Pick a remaining element.
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex], array[currentIndex]];
+	}
+
+	return array;
+}
+
 const Questions = () => {
 
 	const [questions, setQuestions] = useState([]);
@@ -26,7 +44,8 @@ const Questions = () => {
 			let path = '/result';
 			navigate(path, {
 				state: {
-					score: scoreList
+					score: scoreList,
+					questionlist: questions
 				}
 			});
 		}
@@ -42,12 +61,24 @@ const Questions = () => {
 				const scenario = collectionDoc.data().Background;
 				let subCollectionDocs = await getDocs(query(collection(db, "Scenario", collectionDoc.id, "Questions"), orderBy("Questionno")));
 				subCollectionDocs.forEach(subCollectionDoc => {
-					temp.push({
-						scenario: scenario,
-						question: subCollectionDoc.data().Question,
-						answerOptions: subCollectionDoc.data().AnswerOptions,
-						questionno: subCollectionDoc.data().Questionno
-					})
+					if (subCollectionDoc.data().shuffle) {
+						temp.push({
+							scenario: scenario,
+							question: subCollectionDoc.data().Question,
+							answerOptions: shuffle(subCollectionDoc.data().AnswerOptions),
+							questionno: subCollectionDoc.data().Questionno,
+							rationale: subCollectionDoc.data().rationale
+						})
+					} else {
+						temp.push({
+							scenario: scenario,
+							question: subCollectionDoc.data().Question,
+							answerOptions: subCollectionDoc.data().AnswerOptions,
+							questionno: subCollectionDoc.data().Questionno,
+							rationale: subCollectionDoc.data().rationale
+						})
+					}
+					
 				});
 				setQuestions(temp);
 			});
@@ -65,32 +96,32 @@ const Questions = () => {
 					<img className="fixed top-30 left-0 " src={require('../images/ellipse-1.png')} alt="" />
 					{/* Left part */}
 					{questions && questions.length > 0 &&
-					<div className="col-span-2 questions-parent max-h-screen md:h-screen ">
-						<div className='scenario-questions-card glass-bg'>
-							<Question question={questions[currentQuestion]} />
+						<div className="col-span-2 questions-parent max-h-screen md:h-screen ">
+							<div className='scenario-questions-card glass-bg'>
+								<Question question={questions[currentQuestion]} />
+							</div>
 						</div>
-					</div>	
 					}
 
 					{/* Right part */}
 					{questions && questions.length > 0 &&
-					<div className="col-span-3 options-parent max-h-screen">
-						<div className="grid grid-rows-6 gap-10 min-w-full">
-							<div className="row-span-2 primary-questions-card glass-bg">
-								<h3 className="text-black md:text-xl mb-2">{questions[currentQuestion].question}</h3>
-							</div>
-							<div className="row-span-1 flex items-center justify-center text-white">
-								<p>Please select an answer below </p>
-							</div>
+						<div className="col-span-3 options-parent max-h-screen">
+							<div className="grid grid-rows-6 gap-10 min-w-full">
+								<div className="row-span-2 primary-questions-card glass-bg">
+									<h3 className="text-black md:text-xl mb-2">{questions[currentQuestion].question}</h3>
+								</div>
+								<div className="row-span-1 flex items-center justify-center text-white">
+									<p>Please select an answer below </p>
+								</div>
 
-							<div className="row-span-3 options-questions-card glass-bg-2">
-								<QuizOptions options={questions[currentQuestion]} onSelectOption={(score) => { handleAnswerSelect(score) }} />
+								<div className="row-span-3 options-questions-card glass-bg-2">
+									<QuizOptions options={questions[currentQuestion]} onSelectOption={(score) => { handleAnswerSelect(score) }} />
+								</div>
 							</div>
 						</div>
-					</div>	
 					}
 
-			
+
 
 				</motion.div>
 			</section>
--- interactiveai/src/views/Result.js
+++ interactiveai/src/views/Result.js
@@ -1,22 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { motion } from 'framer-motion';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Slider from "react-slick";
 import ResultPopup from '../components/ResultPopup';
-import { db } from '../index'
 import '../assets/styles.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
 
 const Result = (props) => {
-
-  const [questionRationale, setQuestionRationale] = useState([]);
+  
   const [clickedQuestion, setClickedQuestion] = useState([]);
   const [question, setQuestionno] = useState(1);
   const [showPopUp, setShowPopUp] = useState(false);
   const { state } = useLocation();
-  const [score, setScore] = useState( () => state.score.reduce((a, b) => a + b, 0));
+  const questionRationale = state.questionlist;
+  const [score, setScore] = useState(() => state.score.reduce((a, b) => a + b, 0));
   const navigate = useNavigate();
   const settings = {
     dots: true,
@@ -27,51 +25,27 @@ const Result = (props) => {
     adaptiveHeight: true
   };
 
-  useEffect(() => {
-		const getQuestions = async () => {
-			const temp = [];
-			const questionsCollectionRef = collection(db, "Scenario")
-			const documents = await getDocs(query(questionsCollectionRef, orderBy("Scenariono")));
-
-			documents.forEach(async collectionDoc => {
-				const scenario = collectionDoc.data().Background;
-				let subCollectionDocs = await getDocs(query(collection(db, "Scenario", collectionDoc.id, "Questions"), orderBy("Questionno")));
-				subCollectionDocs.forEach(subCollectionDoc => {
-					temp.push({
-						scenario: scenario,
-						question: subCollectionDoc.data().Question,
-						answerOptions: subCollectionDoc.data().AnswerOptions,
-						questionno: subCollectionDoc.data().Questionno,
-            rationale: subCollectionDoc.data().rationale,
-					})
-				});
-				setQuestionRationale(temp);
-			});
-		}
-		getQuestions();
-	}, [])
-
   const renderFeedback = () => {
 
     if (score <= 4) {
       return <>
-      <h2 className="text-2xl md:text-2xl lg:text-2xl uppercase mb-16">You suck</h2>
-      <p className="text-xl md:text-xl lg:text-xl mb-10">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <h2 className="text-2xl md:text-2xl lg:text-2xl uppercase mb-16">You suck</h2>
+        <p className="text-xl md:text-xl lg:text-xl mb-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </>
     }
     else if (score <= 8) {
-      return <> 
-      <h2 className="text-2xl md:text-2xl lg:text-2xl uppercase mb-16">You did ok</h2>
-      <p className="text-xl md:text-xl lg:text-xl mb-10">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      return <>
+        <h2 className="text-2xl md:text-2xl lg:text-2xl uppercase mb-16">You did ok</h2>
+        <p className="text-xl md:text-xl lg:text-xl mb-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </>
     }
     else {
       return <>
-      <h2 className="text-2xl md:text-2xl lg:text-2xl uppercase mb-16">You're literally hacking</h2>
-      <p className="text-xl md:text-xl lg:text-xl mb-10">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <h2 className="text-2xl md:text-2xl lg:text-2xl uppercase mb-16">You're literally hacking</h2>
+        <p className="text-xl md:text-xl lg:text-xl mb-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </>
     }
   }
@@ -79,9 +53,9 @@ const Result = (props) => {
   const showExplanation = (questionno) => {
     setQuestionno(questionno);
     setShowPopUp(true);
-    for(let i = 0;  i< questionRationale.length; i++){
-      let indexRationale =  i+1;
-      if(questionno.match(String(indexRationale))){
+    for (let i = 0; i < questionRationale.length; i++) {
+      let indexRationale = i + 1;
+      if (questionno.match(String(indexRationale))) {
         setClickedQuestion(questionRationale[i]);
       }
     }
@@ -94,10 +68,10 @@ const Result = (props) => {
     const ques3 = String(questionNo + 2);
     return (
       <div className="slider-content">
-      <button className="result-btn w-5/6 mb-4 text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12" onClick={() => showExplanation(ques1)}>Question {ques1}</button>
-      <button className="result-btn w-5/6 mb-4 text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12" onClick={() => showExplanation(ques2)}>Question {ques2}</button>
-      <button className="result-btn w-5/6 mb-4 text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12" onClick={() => showExplanation(ques3)}>Question {ques3}</button>
-    </div>
+        <button className="result-btn w-5/6 mb-4 text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12" onClick={() => showExplanation(ques1)}>Question {ques1}</button>
+        <button className="result-btn w-5/6 mb-4 text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12" onClick={() => showExplanation(ques2)}>Question {ques2}</button>
+        <button className="result-btn w-5/6 mb-4 text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12" onClick={() => showExplanation(ques3)}>Question {ques3}</button>
+      </div>
     )
   }
 
@@ -106,11 +80,11 @@ const Result = (props) => {
     navigate(path);
   }
 
-  const quesNos =  [1,4,7,10,13];
+  const quesNos = [1, 4, 7, 10, 13];
   return (
     <React.Fragment>
       <section>
-        <ResultPopup showPopUp={showPopUp} setShowPopUp={setShowPopUp} content={question} score={score} clickedQuestion={clickedQuestion}/>
+        <ResultPopup showPopUp={showPopUp} setShowPopUp={setShowPopUp} content={question} score={score} clickedQuestion={clickedQuestion} />
         <motion.div className='w-screen h-screen grid grid-rows-2 text-black text-4xl md:grid-rows-2' initial={{ width: 0 }} animate={{ width: "100%" }} exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}>
           <div className=' w-full h-full centered md:w-screen flex flex-col text-center px-6 pt-5'>
             <div className="flex-1 text-black" style={{ display: 'block', alignItems: 'center' }}>
@@ -132,12 +106,12 @@ const Result = (props) => {
               <main className="feedback container mx-auto px-6 pt-16 flex-1 text-center">
                 {renderFeedback()}
                 <button
-                            type="submit"
-                            value="Let's start!"
-                            name="member[submit]"
-                            id="member_submit"
-                            className="start-btn"
-                            onClick={tryAgainButton}>Try Again!</button>
+                  type="submit"
+                  value="Let's start!"
+                  name="member[submit]"
+                  id="member_submit"
+                  className="start-btn"
+                  onClick={tryAgainButton}>Try Again!</button>
               </main>
             </div>
           </div>
